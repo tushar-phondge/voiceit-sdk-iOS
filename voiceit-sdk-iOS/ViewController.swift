4c53ac2f53865a9ceac829757e0e447d155751dc
Armaan Bindra <armaanbindra@Armaans-MacBook-Pro.local>
2015-08-28 11:10:48 -0500

    First Commit of Blank Project

---
+++ voiceit-sdk-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  voiceit-sdk-iOS
+//
+//  Created by Armaan Bindra on 8/28/15.
+//  Copyright (c) 2015 VoiceIt Technologies LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
