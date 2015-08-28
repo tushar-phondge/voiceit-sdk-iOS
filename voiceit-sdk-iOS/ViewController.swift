2c6abfce7ed453af3d5f958ff2a67e34bd86d854
Armaan Bindra <armaanbindra@Armaans-MacBook-Pro.local>
2015-08-28 14:44:21 -0500

    Added Place for Developer Id So Developers Can Download and Use the Demo Now

--- voiceit-sdk-iOS/ViewController.swift
+++ voiceit-sdk-iOS/ViewController.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //ADD YOUR VOICEIT DEVELOPER ID HERE for Example: 123456, If you do not already have a DEVELOPER ID get one 
+    //at https://siv.voiceprintportal.com/getDeveloperID.jsp
+    
+    let DEVELOPER_ID = "DEVELOPER_ID_HERE"
 
+    @IBOutlet weak var userTokenField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func loginClicked(sender: AnyObject) {
+        let vc = VoiceItLogin(DEVELOPER_ID,userToken: userTokenField.text!,vpPhrase: "Never forget tomorrow is a new day", textConfidence: "30.0", confidence: "89" , callback: { (result) -> Void in
+            println("Result is \(result)");
+            if result == "Success"
+            {
+                self.resultLabel.text = "Login Success";
+            }
+            else if result == "Failed"
+            {
+                self.resultLabel.text = "Login Failed";
+            }
+            
+        })
+        self.presentViewController(vc, animated: true, completion: nil)
     }
 
 
